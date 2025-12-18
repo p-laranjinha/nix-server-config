df67acabda41ad74c40f4c4159dc81e40632cb76
p-laranjinha <plcasimiro2000@gmail.com>
2025-12-18 15:23:37 +0000

    Removed most chown errors in swag.

--- modules/containers/swag/default.nix
+++ modules/containers/swag/default.nix
@@ -8,6 +8,8 @@
   localVars = vars.containers.containers.swag;
   swagImage = "lscr.io/linuxserver/swag:5.2.2";
   swagConfigDir = "${vars.containers.dataDir}/swag/config";
+  swagContainerPUID = "1000";
+  swagHostPUID = toString ((lib.toInt swagContainerPUID) + vars.containers.uidGidCount * localVars.i + (builtins.elemAt config.users.users.${vars.username}.subUidRanges 0).startUid);
 in {
   options.opts.containers.swag = {
     enable = lib.mkEnableOption "SWAG";
@@ -18,9 +20,10 @@ in {
     systemd.tmpfiles.rules = [
       "d ${vars.containers.dataDir}/swag 2770 ${vars.username} ${localVars.mainGroup} - -"
       "d ${swagConfigDir} 2770 ${vars.username} ${localVars.mainGroup} - -"
-      "d ${swagConfigDir}/dns-conf 2770 ${vars.username} ${localVars.mainGroup} - -"
+      "Z ${swagConfigDir}/* 770 ${swagHostPUID} ${localVars.mainGroup} - -"
+      # Symlinks aren't created if the target directory has a different owner.
+      "d ${swagConfigDir}/dns-conf 770 ${vars.username} ${localVars.mainGroup} - -"
       "L+ ${swagConfigDir}/dns-conf/porkbun.ini - - - - ${config.secrets.certbot-porkbun.path}"
-      "Z ${swagConfigDir} 2770 ${vars.username} ${localVars.mainGroup} - -"
     ];
     secrets.certbot-porkbun = {
       sopsFile = ./porkbun.ini;
@@ -45,6 +48,7 @@ in {
                 "80:80"
               ];
               environments = {
+                PUID = swagContainerPUID;
                 PGID = funcs.containers.getContainerGid localVars.mainGroup;
                 TZ = "Europe/Lisbon";
                 URL = "orangepebble.net";
