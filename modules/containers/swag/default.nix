424afa8d134c7a6a1551e2328db10aa54e8bd111
p-laranjinha <plcasimiro2000@gmail.com>
2025-12-22 02:19:38 +0000

    Added Cloudflare Tunnel and other smaller changes.

--- modules/containers/swag/default.nix
+++ modules/containers/swag/default.nix
@@ -9,8 +9,10 @@
   swagImage = "lscr.io/linuxserver/swag:5.2.2";
   configDir = funcs.relativeToAbsoluteConfigPath ./config;
   defaultConfigDir = "${vars.containers.dataDir}/swag/config";
+  modCacheDir = "${vars.containers.dataDir}/swag/modcache";
   symlinks = {
     "${defaultConfigDir}/dns-conf/porkbun.ini" = toString config.secrets.certbot-porkbun.path;
+    "${defaultConfigDir}/tunnelconfig.yml" = "${configDir}/tunnelconfig.yml";
     "${defaultConfigDir}/nginx/site-confs/default.conf" = "${configDir}/default.conf";
     "${defaultConfigDir}/nginx/nginx.conf" = "${configDir}/nginx.conf";
     "${defaultConfigDir}/nginx/proxy-confs/dashboard.subdomain.conf" = "${configDir}/dashboard.subdomain.conf";
@@ -34,6 +36,7 @@ in {
       [
         "d ${vars.containers.dataDir}/swag 2770 ${vars.username} ${localVars.mainGroup} - -"
         "d ${defaultConfigDir} 2770 ${vars.username} ${localVars.mainGroup} - -"
+        "d ${modCacheDir} 2770 ${vars.username} ${localVars.mainGroup} - -"
         "Z ${defaultConfigDir}/* 770 ${hostPUID} ${localVars.mainGroup} - -"
         # Symlinks aren't created if the destination directories have a different owner.
         "d ${defaultConfigDir}/dns-conf 770 ${vars.username} ${localVars.mainGroup} - -"
@@ -45,15 +48,29 @@ in {
         "Z ${configDir}/* 770 ${vars.username} ${localVars.mainGroup} - -"
       ]
       ++ (map (x: "L+ ${x.name} - - - - ${x.value}") (lib.attrsToList symlinks));
-    secrets.certbot-porkbun = {
-      sopsFile = ./porkbun.ini;
-      format = "ini";
-      # Entire file.
-      key = "";
-      # Only the user and group can read and nothing else.
-      mode = "0440";
-      owner = vars.username;
-      group = localVars.mainGroup;
+    secrets = builtins.mapAttrs (_: value:
+      {
+        format = "binary";
+        # Entire file.
+        key = "";
+        # Only the user and group can read and nothing else.
+        mode = "0440";
+        owner = vars.username;
+        group = localVars.mainGroup;
+      }
+      // value) {
+      # Random string:
+      # `LC_ALL=C tr -dc 'A-Za-z0-9!#%&'\''()*+,-./:;<=>?@[\\]^_{|}~' </dev/urandom | head -c 32`
+      # Automatically encrypt string with sops:
+      # `sops --input-type binary -e <(LC_ALL=C tr -dc 'A-Za-z0-9!#%&'\''()*+,-./:;<=>?@[\\]^_{|}~' </dev/urandom | head -c 32) > <file path>`
+      certbot-porkbun = {
+        sopsFile = ./secrets/porkbun.ini;
+        format = "ini";
+      };
+      swag-cloudflare = {
+        sopsFile = ./secrets/cloudflare.env;
+        format = "dotenv";
+      };
     };
     networking.firewall.allowedTCPPorts = [80 443];
     hm = let
@@ -93,11 +110,17 @@ in {
                   SUBDOMAINS = "wildcard";
                   VALIDATION = "dns";
                   DNSPLUGIN = "porkbun";
-                  DOCKER_MODS = "linuxserver/mods:swag-dashboard|linuxserver/mods:swag-dbip";
+                  DOCKER_MODS = "linuxserver/mods:swag-dashboard|linuxserver/mods:swag-dbip|linuxserver/mods:universal-cloudflared";
+                  # https://www.linuxserver.io/blog/zero-trust-hosting-and-reverse-proxy-via-cloudflare-swag-and-authelia
+                  CF_TUNNEL_NAME = "server";
+                  FILE__CF_TUNNEL_CONFIG = "/config/tunnelconfig.yml";
                 };
+                environmentFiles = [(toString config.secrets.swag-cloudflare.path)];
+                addHosts = ["orangepebble.net:127.0.0.1"];
                 volumes =
                   [
                     "${defaultConfigDir}:/config"
+                    "${modCacheDir}:/modcache"
                   ]
                   ++ (map (x: "${x}:${x}") (builtins.attrValues symlinks));
                 networks = builtins.attrValues networks;
